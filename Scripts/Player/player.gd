0346d67451296713a1ded56fb64cd36bd4c95b25
The-Only-A_Games <theonlyagames@gmail.com>
2025-08-25 12:14:20 +0200

    Trying to fix the dash mechanic

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -9,6 +9,7 @@ const JUMP =  -500
 ## Dash attributes
 var dash = 1
 var dash_time = 0
+var chargeDash = 0.5
 const DASH_LIMIT = 0.5
 
 ## Tools
@@ -27,17 +28,24 @@ func _physics_process(delta):
 		velocity.y = JUMP
 	
 	# Increase value of dash when dash key is clicked
-	if Input.is_action_pressed("dash"):
+	if Input.is_action_pressed("dash") and chargeDash > 0:
 		dash_time += delta
+		chargeDash -= delta
 		dash = 3
+		
+		# Stop dash if exceeded time or charge
+		if dash_time >= DASH_LIMIT or chargeDash <= 0:
+			dash = 1
+			dash_time = 0
 	
 	# Reset DASH values when button is released
 	elif Input.is_action_just_released("dash"):
 		dash = 1
 		dash_time = 0
 	
-	if dash_time >= DASH_LIMIT: # Limits dash
-		dash = 1
+	
+	if dash == 1 and chargeDash < 3.0:
+		chargeDash += 1.0 * delta
 	
 	velocity.x = direction * SPEED * dash
 	move_and_slide()
